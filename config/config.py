f63eb210362f32a611bdff62bf87ed1b47c69cd1
david <david@muxi.at>
2025-12-18 16:30:17 +0100

    remove logs from git

--- config/config.py
+++ config/config.py
@@ -30,24 +30,24 @@ class Configuration:
 
         size = self.config.get('simulation', 'size', fallback=self.DEFAULTS['simulation']['size'])
         self.width = self.config.get('simulation', 'width', fallback=size)
-        logger.debug(f"config.width = {self.width}")
         self.height = self.config.get('simulation', 'height', fallback=size)
-        logger.debug(f"config.height = {self.height}")
         self.sim_seed = seed if seed else self.config.get('simulation', 'seed', fallback=self.DEFAULTS['simulation']['seed'])
+        logger.debug(f"config.width = {self.width}")
+        logger.debug(f"config.height = {self.height}")
         logger.debug(f"config.sim_seed = {self.sim_seed}")
 
         self.preset_source = self.config.get('preset', 'source', fallback=self.DEFAULTS['preset']['source'])
-        logger.debug(f"config.preset_source = {self.preset_source}")
         self.preset_seed = self.config.getint('preset', 'seed', fallback=self.DEFAULTS['preset']['seed'])
-        logger.debug(f"config.preset_seed = {self.preset_seed}")
         self.preset_file = self.config.get('preset', 'file', fallback=self.DEFAULTS['preset']['file'])
+        logger.debug(f"config.preset_source = {self.preset_source}")
+        logger.debug(f"config.preset_seed = {self.preset_seed}")
         logger.debug(f"config.preset_file = {self.preset_file}")
 
         # self.preset = PresetGenerator.get(self.preset_source, self.width, self.height, self.seed, self.preset_file)
 
         self.output_dir = self.config.get('output', 'directory', fallback=self.DEFAULTS['output']['directory'])
-        logger.debug(f"config.output_dir = {self.output_dir}")
         self.output_pattern = self.config.get('output', 'pattern', fallback=self.DEFAULTS['output']['pattern'])
+        logger.debug(f"config.output_dir = {self.output_dir}")
         logger.debug(f"config.output_pattern = {self.output_pattern}")
 
     def __str__(self) -> str:
