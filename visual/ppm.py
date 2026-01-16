31d0aee9d37a211ce16b797bf720cd70229509cd
david <david@muxi.at>
2026-01-16 12:05:26 +0100

    add cell state visualizer via PPM format

--- visual/ppm.py
+++ visual/ppm.py
@@ -1,5 +1,8 @@
 from .visualizer import Visualizer
 
 class PPM(Visualizer):
-    def __init__(self):
-        pass
+    @classmethod
+    def write_ppm(cls, outfile, width, height, pixels):
+        outfile.write(f'P3\n{width} {height}\n')
+        for pixel in pixels:
+            outfile.write(" ".join(pixel))
--- visual/visualizer.py
+++ visual/visualizer.py
@@ -1,4 +1,45 @@
+import os
+from pathlib import Path
+from .ppm import PPM
 
 class Visualizer:
-    def get(self):
+    def __init__(self, config):
+        self.config = config 
+        self.frame_id = 0
+
+    def get_output_path(self):
+        output_dir = Path(self.config.output_dir)
+        if not output_dir.exists():
+            os.mkdir(output_dir)
+        return output_dir / Path(self.config.output_pattern.format(self.frame_id))
+
+    def write_frame(self, state):
+        self.frame(state)
+        self.frame_id += 1
+
+    def frame(self, state):
         pass
+
+
+COLOR_MAP = {
+    0: [255, 0, 0], 
+    1: [0, 0, 0],
+    2: [255, 165, 0],
+    3: [0, 255, 0], 
+}
+
+# Visualizers for every variable:
+#     - cell state 
+#     - oxygen
+#     - fuel
+#     - heat
+
+class PPMCellStateVisualizer(Visualizer):
+    def frame(self, state):
+        width = self.config.width
+        height = self.config.height
+        cell_state = state.cell_state.flatten()
+        cell_colors = [COLOR_MAP[x] for x in cell_state]
+        ppm = PPM()
+        with open(self.get_output_path(), "w") as outfile:
+            ppm.write_ppm(outfile, width, height, cell_colors)
